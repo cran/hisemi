4e02dea79540127e16454cce8519b37fe761fdb1
Long Qu <longorognol@hotmail.com>
2012-02-24 00:00:00 +0000

    version 1.0-208

--- R/EM.Newton.support.R
+++ R/EM.Newton.support.R
@@ -46,7 +46,7 @@ plotHisemitResid=function(obj, y.type=c('hist','scatter'), x.type=c('lfdr','pi0'
 }
 
 plotHisemitTuning=function(obj, SE=FALSE, add=FALSE,  ...)
-{   if(length(obj$spar$all)<2) stop('need enough smoothing parameters')
+{   if(length(setdiff(obj$spar$all,Inf))<1) stop('need finite smoothing parameters')
     tuning.method=obj$tuning$method
     spar.exp=log10(obj$spar$all)
     final.spar.exp=log10(obj$spar$final)
@@ -69,7 +69,7 @@ plotHisemitTuning=function(obj, SE=FALSE, add=FALSE,  ...)
     myplot=if(add)lines else plot
 
     crit.range=range(criterion.mean.mean, na.rm=TRUE)*G
-    enp.range=c(2, max(obj$enp$logistic))
+    enp.range=c(2+ncol(obj$model$x)*(obj$model$pen.order-1), max(obj$enp$logistic))
             
     if(!add){    par(mar=c(5,4,4,4)+.1)}
 
@@ -81,11 +81,11 @@ plotHisemitTuning=function(obj, SE=FALSE, add=FALSE,  ...)
         for(i in 1:length(spar.exp))
             lines(c(spar.exp[i],spar.exp[i]),G*(criterion.mean.mean[i]+c(1,-1)*criterion.var.se[i]),col=2,lwd=3)
         abline(v=spar.exp[imin.cv])
-#        spar.exp[criterion.mean.mean<(criterion.mean.mean[imin.cv]+criterion.var.se[imin.cv])]=Inf
-#        cv.1se=tail(which.max(spar.exp),1)
-#        abline(v=spar.exp[cv.1se],lty=2)
-#        axis(1,spar.exp[cv.1se])
-#    abline(h=criterion.mean.mean[imin.cv]+criterion.var.se[imin.cv])
+    #        spar.exp[criterion.mean.mean<(criterion.mean.mean[imin.cv]+criterion.var.se[imin.cv])]=Inf
+    #        cv.1se=tail(which.max(spar.exp),1)
+    #        abline(v=spar.exp[cv.1se],lty=2)
+    #        axis(1,spar.exp[cv.1se])
+    #    abline(h=criterion.mean.mean[imin.cv]+criterion.var.se[imin.cv])
     }else{
 
             myplot(spar.exp,G*drop(criterion.mean.mean), ylab=if(add)''else tuning.method, xlab='log10(smoothing par.)',
@@ -110,7 +110,7 @@ plotHisemitTuning=function(obj, SE=FALSE, add=FALSE,  ...)
 }
 
 
-coefficients.hisemit=coef.hisemit=function(object, scale.parameterization=c('r','scale.factor','sd.ncp'), ...)
+coef.hisemit=function(object, scale.parameterization=c('r','scale.factor','sd.ncp'), ...)
 {
     scale.parameterization=match.arg(scale.parameterization)
     ans=c(if(scale.parameterization=='r') {
@@ -125,7 +125,7 @@ coefficients.hisemit=coef.hisemit=function(object, scale.parameterization=c('r',
     ans
 }
 
-fitted.hisemit=fitted.values.hisemit=function(object, fitted.type=c('lfdr','fpp','pi0','f'), gene.list, component, ...)
+fitted.hisemit=function(object, fitted.type=c('lfdr','fpp','pi0','f'), gene.list, component, ...)
 {
     fitted.type=match.arg(fitted.type)
     if(fitted.type=='lfdr')        return(object$lfdr)
@@ -133,7 +133,7 @@ fitted.hisemit=fitted.values.hisemit=function(object, fitted.type=c('lfdr','fpp'
         l=object$lfdr
         ord.l=order(l)
         r=1-logit.inv(cumsum(logit(1-l[ord.l]))/(seq(along=l)))
-        ans=numeric(length(l))
+        ans=l
         ans[ord.l]=r
         return (ans)
     }
@@ -141,9 +141,10 @@ fitted.hisemit=fitted.values.hisemit=function(object, fitted.type=c('lfdr','fpp'
     if (fitted.type=='f') {
         if(missing(component))  return(object$fit$f)
         if(!is.na(pmatch(component,'intercept')) || (length(component)==1 && component==0)) { 
-            return(rep(object$fit$intercept, length(object$fit$lfdr)))
+            return(rep(object$fit$intercept, length(object$lfdr)))
         }
         if (is.numeric(component)) {
+            stopifnot(all(round(component)<=ncol(object$fit$f.covariate)))
             component=sort(unique(  pmax(pmin(round(component), ncol(object$fit$f.covariate)),0) ))
             if(length(component)==0) stop("invalid component")
             if(component[1]==0){ 
@@ -156,7 +157,7 @@ fitted.hisemit=fitted.values.hisemit=function(object, fitted.type=c('lfdr','fpp'
             return(drop(cbind(ans, ans.rest)))
         }
         {
-            warning("component argument is unknown. overall fit is return")
+            warning("component argument is unknown. overall fit is returned")
             return(object$fit$f)
         }
     }
@@ -165,7 +166,7 @@ fitted.hisemit=fitted.values.hisemit=function(object, fitted.type=c('lfdr','fpp'
     }
 }
 
-resid.hisemit=residuals.hisemit=function(object, residual.type='deviance', ...)
+residuals.hisemit=function(object, residual.type='deviance', ...)
 {
     residual.type=match.arg(residual.type)
     if(residual.type=='deviance'){
@@ -233,6 +234,7 @@ confint.hisemit=function(object, parm=c('lfdr', 'fpp', 'beta', 'scale.fact','sd.
         var.beta=diag(V)[-1]
         cl.beta=coef(object)[-1]+outer(t.1malpha*sqrt(var.beta), c(-1,1))
         colnames(cl.beta)=paste(round(c(1-0.5-level/2, 0.5+level/2)*100, 3), '%',sep='')
+        rownames(cl.beta)=names(coef(object))[-1]
         return(cl.beta)
     }
     if(parm=='scale.fact'){
@@ -259,26 +261,28 @@ confint.hisemit=function(object, parm=c('lfdr', 'fpp', 'beta', 'scale.fact','sd.
         var.coef=diag(V)
         cl.coef=coef(object)+outer(t.1malpha*sqrt(var.coef), c(-1,1))
         colnames(cl.coef)=paste(round(c(1-0.5-level/2, 0.5+level/2)*100, 3), '%',sep='')
+        rownames(cl.coef)=names(coef(object))
         return(cl.coef)
     }
     if(parm=='f'){
-        if(!is.na(pmatch(component,'intercept')) || (length(component)==1 && component==0)) { 
-            return(rep(1,length(object$model$tstat))%o%confint(object, 'beta')[1,])
-        }
         if(missing(component)){
             cov.idx=seq_len(ncol(object$fit$H))
             fitted.f=fitted(object, 'f')
-        }
-        if (is.numeric(component)) {
-            component=sort(unique(  pmax(pmin(round(component), ncol(object$fit$f.covariate)),0) ))
-            if(length(component)!=1) stop("invalid component")
-            if(component[1]==0){ return(rep(1,length(object$model$tstat))%o%confint(object, 'beta')[1,]) }
-            cov.idx=which(object$fit$covariate.idx==component)
-            fitted.f=fitted(object, 'f', component=component)
+        }else {
+            if(!is.na(pmatch(component,'intercept')) || (length(component)==1 && component==0)) { 
+                return(rep(1,length(object$model$tstat))%o%confint(object, 'beta')[1,])
+            }
+            if (is.numeric(component)) {
+                component=sort(unique(  pmax(pmin(round(component), ncol(object$fit$f.covariate)),0) ))
+                if(length(component)!=1) stop("invalid component")
+                if(component[1]==0){ return(rep(1,length(object$model$tstat))%o%confint(object, 'beta')[1,]) }
+                cov.idx=which(object$fit$covariate.idx==component)# this does not count the first scale parameter
+                fitted.f=fitted(object, 'f', component=component)
+            }
         }
         chol.V=chol(nearPD(V)$mat)
         grad=cBind(0, object$fit$H)
-        grad[,-cov.idx]=0
+        grad[,-cov.idx-1L]=0
         gradL=tcrossprod(grad,chol.V)
         var.f=rowSums(gradL*gradL)
         CL.f=fitted.f+outer( t.1malpha*sqrt(var.f), c(-1,1) )
@@ -299,7 +303,7 @@ confint.hisemit=function(object, parm=c('lfdr', 'fpp', 'beta', 'scale.fact','sd.
         return(CL.pi0)
     }
     if(parm=='fpp') {
-
+        stop('not yet implemented')
     }
 
     {
